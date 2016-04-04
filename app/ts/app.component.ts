2463d3504f7b3f761cd263ee2f4e51f77cc9ecd3
Marcel Schmidt <Marcel Schmidt>
2016-04-04 23:17:45 +0200

    Initial Commit.

---
+++ app/ts/app.component.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'app',
+    template: '<h1>Angular 2</h1><button class="btn btn-primary">Test</button>'
+})
+export class AppComponent { }
